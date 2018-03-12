da172edcfb4456b1f71c63c149fd647887291105
O Chicken <Ma.Seoyin@gmail.com>
2018-03-12 23:11:14 +0800

    Add Function.py and Main.py
    You can run the project on Linux

---
+++ Function.py
@@ -0,0 +1,173 @@
+# coding=utf-8
+# author: Ma Seoyin, 2014 Applied Physics, SCUT
+from numpy import *
+from scipy import stats
+from scipy.interpolate import interp1d
+import scipy.constants as C
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+#   Randomly generate parameters in parameters' space
+#   ----------------------------------------------------------------------------------------------------------------
+def GeneratePara(t, Para0, D, P):
+    # [E_s^{-1},   C+ / C-,   C_s / C-,   gamma- - gamma+,   gamma- - gamma_s] -------------------------------------
+    Lolims = array([17.916, 0.6])  # Parameters' space, lower limits
+    Uplims = array([18.229, 1])  # Parameters' space, upper limits
+    #  In the t = 0 & 1 processes, generate Para from a large given range ------------------------------------------
+    if t <= 1:
+        Para1 = random.uniform(Lolims, Uplims, 2)
+    # In the t >= 2 processes, generate Para from the previous result, according to the Gaussian Distribution ------
+    else:
+        Para1 = Para0 + array(matrix(P) * matrix(D) * transpose(matrix(random.normal(size=2 * 1)))).flatten()
+        while (Para1 < Lolims).any() or (Uplims < Para1).any():
+            Para1 = Para0 + array(matrix(P) * matrix(D) * transpose(matrix(random.normal(size=2 * 1)))).flatten()
+    return Para1
+
+
+#    ---------------------------------------------------------------------------------------------------------------
+#   Convert Cov to D:
+#   ----------------------------------------------------------------------------------------------------------------
+def Cov2D(Cov):
+    Cov = matrix(Cov)
+    P = array(linalg.eig(Cov)[1])  # eigenvectors
+    # square of diagonalized Cov -----------------------------------------------------------------------------------
+    D = array(sqrt([linalg.eig(Cov)[0][i]*identity(len(Cov))[i] for i in range(len(Cov))]))
+    # D = sqrt(linalg.inv(P) * Cov * P)  # square of diagonalized Cov
+    return [D, P]
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+#   Calculate the Hypothetical values, with TOV Confinement
+#   ----------------------------------------------------------------------------------------------------------------
+def xH(Para, EoS):
+    Msunt0 = 1.989E30  # The mass of sun ---------------------------------------------------------------------------
+    z1 = linspace(1E18, 4.5E18, 100)  # Central density: search range ----------------------------------------------
+    col = len(z1)
+    # Construct the new EoS ----------------------------------------------------------------------------------------
+    rho_crit = Para[0]
+    Vs_crit = Para[1]
+    EoS = EoS.copy()
+    rho = EoS[:, 0]
+    P = EoS[:, 1]
+    Interp_EoS = interp1d(rho, P, kind='linear')
+    P_crit = Interp_EoS(rho_crit)
+    min_index = argmin(abs(rho - rho_crit))
+    if rho[min_index] <= rho_crit:
+        min_index += 1
+    P_new = log10((Vs_crit * C.c) ** 2 * (10 ** rho[min_index:] - 10 ** rho_crit) + 10 ** P_crit)
+    P[min_index:] = P_new
+    # Interpolate to the EoS ---------------------------------------------------------------------------------------
+    Interp_EoS = interp1d(rho, P, kind='linear')
+    Interp_EoS_Inv = interp1d(P, rho, kind='linear')
+    # Define -------------------------------------------------------------------------------------------------------
+    zr = array(zeros((1, 1)))
+    p = array(zeros((1, 1)))
+    r = array(zeros((1, 1)))
+    m = array(zeros((1, 1)))
+    pcalculate = array(zeros(col))
+    rsurface = array(zeros(col))
+    Msunt = array(zeros(col))
+    indexmax = array(zeros(col))
+    # Begin to integrate the TOV equation with 4th order Rung-Kutta method
+    x = 1
+    for s in range(col):
+        # Define the iteration parameters --------------------------------------------------------------------------
+        i = -1
+        n = int(1E18)
+        h = 1
+        # Define ---------------------------------------------------------------------------------------------------
+        if s == 1:
+            zr_col_len = shape(zr)[1]
+            zr = vstack((zr, array(zeros((col - 1, zr_col_len)))))
+            p = vstack((p, array(zeros((col - 1, zr_col_len)))))
+            r = vstack((r, array(zeros((col - 1, zr_col_len)))))
+            m = vstack((m, array(zeros((col - 1, zr_col_len)))))
+        zr[s, 0] = z1[s]  # Initial central density ----------------------------------------------------------------
+        z0 = log10(z1[s])
+        p0 = Interp_EoS(z0)
+        p[s, 0] = 10 ** p0
+        r[s, 0] = 1  # Initial radius ------------------------------------------------------------------------------
+        m[s, 0] = (4/3)*pi * zr[s, 0] * (r[s, 0]**3)  # Construct the initial m ------------------------------------
+        p[s, 0] = p[s, 0] - (2/3)*pi * (zr[s, 0] * (C.c**2) + p[s, 0]) * (zr[s, 0] * (C.c**2) + x * 3 * p[s, 0]) * (C.G/(C.c**4) * (r[s, 0]**2))
+        # Begin the cyclic integral --------------------------------------------------------------------------------
+        while i < n:
+            i += 1
+            if s == 0:
+                zr = hstack((zr, array([[0]])))
+                p = hstack((p, array([[0]])))
+                r = hstack((r, array([[0]])))
+                m = hstack((m, array([[0]])))
+            r[s, i + 1] = r[s, i] + h
+            k1 = -(zr[s, i] * C.c**2 + p[s, i]) * (m[s, i] + x * 4 * pi * r[s, i]**3 * p[s, i] / C.c**2) * C.G / (C.c**2) / (r[s, i] * (r[s, i] - 2 * C.G / (C.c**2) * m[s, i]))
+            l1 = 4 * pi * r[s, i]**2 * zr[s, i]
+            p01 = p[s, i] + h * k1 / 2
+            if p01 > 1E-10:
+                p001 = log10(p01)
+                z001 = Interp_EoS_Inv(p001)
+                zr[s, i] = 10**z001
+            k2 = -(zr[s, i] * C.c ** 2 + (p[s, i] + 0.5 * h * k1 / 2)) * ((m[s, i] + 0.5 * h * l1 / 2) + x * 4 * pi * (r[s, i] + h / 2) ** 3 * (p[s, i] + 0.5 * h * k1 / 2) / C.c ** 2) * C.G / (C.c ** 2) / ((r[s, i] + h / 2) * ((r[s, i] + h / 2) - 2 * C.G / (C.c ** 2) * (m[s, i] + 0.5 * h * l1 / 2)))
+            l2 = 4 * pi * (r[s, i] + h / 2) ** 2 * zr[s, i]
+            p02 = p[s, i] + h * k2 / 2
+            if p02 > 1E-10:
+                p002 = log10(p02)
+                z002 = Interp_EoS_Inv(p002)
+                zr[s, i] = 10**z002
+            k3 = -(zr[s, i] * C.c**2 + (p[s, i] + 0.5 * h * k2 / 2)) * ((m[s, i] + 0.5 * h * l2 / 2) + x * 4 * pi * (r[s, i] + h / 2)**3 * (p[s, i] + 0.5 * h * k2 / 2) / C.c**2) * C.G / C.c**2 / ((r[s, i] + h / 2) * ((r[s, i] + h / 2) - 2 * C.G / C.c**2 * (m[s, i] + 0.5 * h * l2 / 2)))
+            l3 = 4 * pi * (r[s, i] + h / 2)**2 * zr[s, i]
+            p03 = p[s, i] + h * k3
+            if p03 > 1E-10:
+                p003 = log10(p03)
+                z003 = Interp_EoS_Inv(p003)
+                zr[s, i] = 10**z003
+            k4 = -(zr[s, i] * C.c**2 + (p[s, i] + h * k3)) * ((m[s, i] + h * l3) + x * 4 * pi * (r[s, i] + h)**3 * (p[s, i] + h * k3) / C.c**2) * C.G / C.c**2 / ((r[s, i] + h) * ((r[s, i] + h) - 2 * C.G / C.c**2 * (m[s, i] + h * l3)))
+            l4 = 4 * pi * (r[s, i] + h)**2 * zr[s, i]
+            p[s, i + 1] = p[s, i] + 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
+            m[s, i + 1] = m[s, i] + 1 / 6 * h * (l1 + 2 * l2 + 2 * l3 + l4)
+            if p[s, i + 1] > 1E-10:
+                p00 = log10(p[s, i + 1])
+                z00 = Interp_EoS_Inv(p00)
+                zr[s, i + 1] = 10**z00
+            else:
+                zr[s, i + 1] = zr[s, i]  # Density (corresponding to the given central density) --------------------
+                pcalculate[s] = p[s, i + 1]  # Surface pressure ----------------------------------------------------
+                p[s, i + 1] = 0  # Define the surface pressure as 0 ------------------------------------------------
+                j = i + 1
+                i = n + 1
+        rsurface[s] = r[s, j]  # Radius of the NS ------------------------------------------------------------------
+        Msunt[s] = m[s, j - 1] / Msunt0  # Mass of the NS (Msun as unit) -------------------------------------------
+        indexmax[s] = j
+    return max(Msunt)  # Return the rho (density), r (radius), and M --------------------------------
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+#   Calculate the chi^2: Hypothetical values and Data
+#   ----------------------------------------------------------------------------------------------------------------
+def chi2(Para, Data, EoS):
+    xD = Data[0]  # Observational mass 2.01
+    sigma = Data[1]
+    Chi2 = sum((xH(Para, EoS) - xD)**2 / sigma**2)
+    return Chi2
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+#   Confidence interval
+#   ----------------------------------------------------------------------------------------------------------------
+def CI(df, chi2):
+    re = 1 - stats.chi2.cdf(chi2, df=df)
+    return re
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+#   Confidence interval index
+#   ----------------------------------------------------------------------------------------------------------------
+def CIindex(CI, Accept):
+    Len = len(Accept)
+    N = int(floor(sum(Accept) / CI))
+    ProbabilityDensity = Accept / N  # Sum ProbabilityDensity will obtain CI
+    Probability = array([sum(ProbabilityDensity[i:]) for i in range(Len)])
+    CI_Normal_1sigma = 1-2*(1-stats.norm.cdf(1, 0, 1))
+    i = 0
+    while Probability[i] > CI_Normal_1sigma:
+        i += 1
+    index = i - 1
+    return [index, ProbabilityDensity, Probability]
---
+++ Main.py
@@ -0,0 +1,145 @@
+# coding=utf-8
+# author: Ma Seoyin, 2014 Applied Physics, SCUT
+"""Remember to delete 'winsound' from the import and comment the last code containing of 'winsound.Beep()'
+   if you want to run me on a Linux machine"""
+from numpy import *
+from itertools import product
+import os, time, winsound, Function, MCMC
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+#   Find the Para_Uplims and Para_Lolims that enabling the y^Theo to be draw in upper and lower limits
+#   ----------------------------------------------------------------------------------------------------------------
+def Para_Uplims_Lolims(Data, Para, Deviation):
+    ParaLen = len(Para)
+    Sign = ['+', '-']
+    Para_uplims_lolims = array([Para] * (2**ParaLen))
+    Y_uplims = ones(2**ParaLen)*1e9
+    Y_lolims = ones(2**ParaLen)*(-1e9)
+    i = 0
+    for Iter in product(range(2), repeat=ParaLen):
+        for j in range(ParaLen):
+            Para_uplims_lolims[i, j] = eval('{}{}{}'.format(Para_uplims_lolims[i, j], Sign[Iter[j]], Deviation[j]))
+        i += 1
+    X = random.uniform(Data[0, 0], Data[-1, 0], 100)
+    for i in range(2**ParaLen):
+        Para_Temp = Para_uplims_lolims[i, :]
+        Yerror = sum(Function.xH(X, Para_Temp, EoS) - Function.xH(X, Para, EoS))
+        if Yerror >= 0:
+            Y_uplims[i] = Yerror
+        if Yerror <= 0:
+            Y_lolims[i] = Yerror
+    Uplims = argmin(Y_uplims)
+    Lolims = argmax(Y_lolims)
+    Para_Uplims = list(Para_uplims_lolims[Uplims, :] - Para)
+    Para_Lolims = list(Para_uplims_lolims[Lolims, :] - Para)
+    for i in range(ParaLen):
+        if Para_Uplims[i] > 0:
+            Para_Uplims[i] = '+'
+            Para_Lolims[i] = '-'
+        else:
+            Para_Uplims[i] = '-'
+            Para_Lolims[i] = '+'
+    return [Para_Uplims, Para_Lolims]
+
+
+#   ----------------------------------------------------------------------------------------------------------------
+# Write Result
+#   ----------------------------------------------------------------------------------------------------------------
+def WriteResult(A, file):
+    if type(A) == ndarray:
+        A = A.tolist()
+    dim = ndim(A)
+    if dim == 0:
+        file.write(str(A))
+    elif dim == 1:
+        for i in range(shape(A)[0]):
+            if type(A[0]) == str:
+                file.write(A[i])
+            else:
+                file.write(str(A[i]) + '\n')
+    elif dim == 2:
+        for i in range(shape(A)[0]):
+            for j in range(shape(A)[1]):
+                file.write(str(A[i][j]) + '\t')
+                if j == range(shape(A)[1])[-1]:
+                    file.write('\n')
+    file.close()
+
+
+DataDir = os.getcwd() + '/'
+Data = loadtxt(DataDir + 'Data.txt')  # Initialize Data
+# rho = linspace(Data[0,0], Data[-1,0], 100)
+# p = linspace(Data[0,1], Data[-1,1], 100)
+# Data = transpose(vstack((rho,p)))
+EoS = loadtxt(DataDir + 'APR.txt')  # APR EoS
+ParaAnswer = loadtxt(DataDir + '/ResultAnswer/' + 'Para.txt')
+
+Start = time.clock()
+T = 3  # The length of MCMC is T
+t = 0
+Para = Function.GeneratePara(t, Para0=None, D=None, P=None)  # Initialize Para
+D = None; P = None  # Get D1 and P1
+# Begin to find the best Para and its relevant parameters D, chi2 Cov etc-------------------------------------------
+t += 1
+ParaSet = array(Para)  # The set of the accepted Para
+chi2 = Function.chi2(Para, Data, EoS)
+chi2Set = array([chi2])  # The set of the chi2 corresponding to the accepted Para
+Accept = array([1])  # The set of the Accept corresponding to the accepted Para
+while t <= T:
+    [Para, chi2, D, P, ParaSet, chi2Set, Accept, mu, Cov] = \
+        MCMC.MarkovChain(Data, EoS, Para, chi2, D, P, ParaSet, chi2Set, Accept, t)
+    t += 1
+CI = Function.CI(df, chi2)
+[index, ProbabilityDensity, Probability] = Function.CIindex(CI, Accept)
+Deviation = abs(Para - ParaSet[index])
+[Para_Uplims, Para_Lolims] = Para_Uplims_Lolims(Data, Para, Deviation)
+End = time.clock()
+TimeUsed = End - Start
+
+# Output Result ---------------------------------------------------------------------------------------------------
+ResultDir = os.getcwd() + '/Result/'
+
+print(TimeUsed)
+file = open(ResultDir + 'TimeUsed.txt', 'w+')
+WriteResult(TimeUsed, file)
+print(chi2)
+file = open(ResultDir + 'chi2.txt', 'w+')
+WriteResult(chi2, file)
+print(CI)
+file = open(ResultDir + 'CI.txt', 'w+')
+WriteResult(CI, file)
+print(Para)
+file = open(ResultDir + 'Para.txt', 'w+')
+WriteResult(Para, file)
+print(Deviation)
+file = open(ResultDir + 'Deviation.txt', 'w+')
+WriteResult(Deviation, file)
+
+file = open(ResultDir + 'D.txt', 'w+')
+WriteResult(D, file)
+file = open(ResultDir + 'P.txt', 'w+')
+WriteResult(P, file)
+file = open(ResultDir + 'ParaSet.txt', 'w+')
+WriteResult(ParaSet, file)
+file = open(ResultDir + 'chi2Set.txt', 'w+')
+WriteResult(chi2Set, file)
+file = open(ResultDir + 'ProbabilityDensity.txt', 'w+')
+WriteResult(ProbabilityDensity, file)
+
+file = open(ResultDir + 'index.txt', 'w+')
+WriteResult(index, file)
+file = open(ResultDir + 'mu.txt', 'w+')
+WriteResult(mu, file)
+file = open(ResultDir + 'Cov.txt', 'w+')
+WriteResult(Cov, file)
+
+print(Para_Uplims)
+file = open(ResultDir + 'Para_Uplims.txt', 'w+')
+WriteResult(Para_Uplims, file)
+print(Para_Lolims)
+file = open(ResultDir + 'Para_Lolims.txt', 'w+')
+WriteResult(Para_Lolims, file)
+
+# Beep when finished -----------------------------------------------------------------------------------------------
+winsound.Beep(2500, 5000)
