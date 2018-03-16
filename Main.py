3379eab478284f630eba69936ac00ee47cace94b
AstrOChicken <Ma.Seoyin@gmail.com>
2018-03-16 22:56:54 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,13 +7,14 @@ from itertools import product
 import os, time, winsound, Function, MCMC
 
 
-#   ----------------------------------------------------------------------------------------------------------------
+#   --------------------------------------------------------------------------------------------------------------------
 #   Find the Para_Uplims and Para_Lolims that enabling the y^Theo to be draw in upper and lower limits
-#   ----------------------------------------------------------------------------------------------------------------
-def Para_Uplims_Lolims(Data, Para, Deviation):
+#   --------------------------------------------------------------------------------------------------------------------
+def Para_Uplims_Lolims(Para, Deviation, EoS, M_NS_max):
     ParaLen = len(Para)
     Sign = ['+', '-']
     Para_uplims_lolims = array([Para] * (2**ParaLen))
+    M_NS_max_lims = zeros(2**ParaLen)
     Y_uplims = ones(2**ParaLen)*1e9
     Y_lolims = ones(2**ParaLen)*(-1e9)
     i = 0
@@ -21,10 +22,11 @@ def Para_Uplims_Lolims(Data, Para, Deviation):
         for j in range(ParaLen):
             Para_uplims_lolims[i, j] = eval('{}{}{}'.format(Para_uplims_lolims[i, j], Sign[Iter[j]], Deviation[j]))
         i += 1
-    X = random.uniform(Data[0, 0], Data[-1, 0], 100)
     for i in range(2**ParaLen):
         Para_Temp = Para_uplims_lolims[i, :]
-        Yerror = sum(Function.xH(X, Para_Temp, EoS) - Function.xH(X, Para, EoS))
+        TOV_output = Function.TOV(Para_Temp, EoS)
+        M_NS_max_lims[i] = max(TOV_output[2])
+        Yerror = M_NS_max_lims[i] - M_NS_max
         if Yerror >= 0:
             Y_uplims[i] = Yerror
         if Yerror <= 0:
@@ -32,7 +34,9 @@ def Para_Uplims_Lolims(Data, Para, Deviation):
     Uplims = argmin(Y_uplims)
     Lolims = argmax(Y_lolims)
     Para_Uplims = list(Para_uplims_lolims[Uplims, :] - Para)
+    M_NS_max_Uplims = M_NS_max_lims[Uplims] - M_NS_max
     Para_Lolims = list(Para_uplims_lolims[Lolims, :] - Para)
+    M_NS_max_Lolims = M_NS_max_lims[Lolims] - M_NS_max
     for i in range(ParaLen):
         if Para_Uplims[i] > 0:
             Para_Uplims[i] = '+'
@@ -40,12 +44,12 @@ def Para_Uplims_Lolims(Data, Para, Deviation):
         else:
             Para_Uplims[i] = '-'
             Para_Lolims[i] = '+'
-    return [Para_Uplims, Para_Lolims]
+    return [Para_Uplims, Para_Lolims, M_NS_max_Uplims, M_NS_max_Lolims]
 
 
-#   ----------------------------------------------------------------------------------------------------------------
+#   --------------------------------------------------------------------------------------------------------------------
 # Write Result
-#   ----------------------------------------------------------------------------------------------------------------
+#   --------------------------------------------------------------------------------------------------------------------
 def WriteResult(A, file):
     if type(A) == ndarray:
         A = A.tolist()
@@ -69,9 +73,6 @@ def WriteResult(A, file):
 
 DataDir = os.getcwd() + '/'
 Data = loadtxt(DataDir + 'Data.txt')  # Initialize Data
-# rho = linspace(Data[0,0], Data[-1,0], 100)
-# p = linspace(Data[0,1], Data[-1,1], 100)
-# Data = transpose(vstack((rho,p)))
 EoS = loadtxt(DataDir + 'APR.txt')  # APR EoS
 ParaAnswer = loadtxt(DataDir + '/ResultAnswer/' + 'Para.txt')
 
@@ -80,7 +81,8 @@ T = 3  # The length of MCMC is T
 t = 0
 Para = Function.GeneratePara(t, Para0=None, D=None, P=None)  # Initialize Para
 D = None; P = None  # Get D1 and P1
-# Begin to find the best Para and its relevant parameters D, chi2 Cov etc-------------------------------------------
+df = 1
+# Begin to find the best Para and its relevant parameters D, chi2 Cov etc-----------------------------------------------
 t += 1
 ParaSet = array(Para)  # The set of the accepted Para
 chi2 = Function.chi2(Para, Data, EoS)
@@ -93,11 +95,14 @@ while t <= T:
 CI = Function.CI(df, chi2)
 [index, ProbabilityDensity, Probability] = Function.CIindex(CI, Accept)
 Deviation = abs(Para - ParaSet[index])
-[Para_Uplims, Para_Lolims] = Para_Uplims_Lolims(Data, Para, Deviation)
+# The NS's maximum mass and its upper limits and lower limits ----------------------------------------------------------
+TOV_output = Function.TOV(Para, EoS)
+M_NS_max = max(TOV_output[2])
+[Para_Uplims, Para_Lolims, M_NS_max_Uplims, M_NS_max_Lolims] = Para_Uplims_Lolims(Para, Deviation, EoS, M_NS_max)
 End = time.clock()
 TimeUsed = End - Start
 
-# Output Result ---------------------------------------------------------------------------------------------------
+# Output Result --------------------------------------------------------------------------------------------------------
 ResultDir = os.getcwd() + '/Result/'
 
 print(TimeUsed)
@@ -115,6 +120,15 @@ WriteResult(Para, file)
 print(Deviation)
 file = open(ResultDir + 'Deviation.txt', 'w+')
 WriteResult(Deviation, file)
+print(M_NS_max)
+file = open(ResultDir + 'M_NS_max.txt', 'w+')
+WriteResult(M_NS_max, file)
+print(M_NS_max_Uplims)
+file = open(ResultDir + 'M_NS_max_Uplims.txt', 'w+')
+WriteResult(M_NS_max_Uplims, file)
+print(M_NS_max_Lolims)
+file = open(ResultDir + 'M_NS_max_Lolims.txt', 'w+')
+WriteResult(M_NS_max_Lolims, file)
 
 file = open(ResultDir + 'D.txt', 'w+')
 WriteResult(D, file)
@@ -141,5 +155,5 @@ print(Para_Lolims)
 file = open(ResultDir + 'Para_Lolims.txt', 'w+')
 WriteResult(Para_Lolims, file)
 
-# Beep when finished -----------------------------------------------------------------------------------------------
+# Beep when finished ---------------------------------------------------------------------------------------------------
 winsound.Beep(2500, 5000)
